79f1db65b4a55f280dab89493bfdbe73596fc512
Ben Loftis <ben@harrisonconsoles.com>
2015-05-29 15:17:46 -0500

    factor out Route::muted_by_others from existing code

--- libs/ardour/route.cc
+++ libs/ardour/route.cc
@@ -1042,6 +1042,17 @@ Route::muted () const
 	return _mute_master->muted_by_self();
 }
 
+bool
+Route::muted_by_others () const
+{
+	//master is never muted by others
+	if (is_master())
+		return false;
+		
+	//now check to see if something is soloed (and I am not)
+	return (_session.soloing() && !self_soloed() && !solo_isolated());
+}
+
 #if 0
 static void
 dump_processors(const string& name, const list<boost::shared_ptr<Processor> >& procs)
